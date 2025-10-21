3097e839c30b717501edb9c64e6cc652b9a9f352
BlazeInferno64 <96913755+BlazeInferno64@users.noreply.github.com>
2025-10-21 17:16:51 +0530

    Update fetch.js

--- public/js/main/fetch.js
+++ public/js/main/fetch.js
@@ -25,7 +25,7 @@ let streaming = false;
 
 //const radioURL = `http://127.0.0.1:3000/api/`;
 
-const radioURL =  `https://radio-station-v2.onrender.com/api/`//`https://radio-station-v2.vercel.app/api/`;
+const radioURL =  `https://radio-station-v2.vercel.app/api/` //`https://radio-station-v2.onrender.com/api/`;
 
 // Build radio URL with optional genre query parameter
 const buildRadioURL = () => {
@@ -221,6 +221,7 @@ if (typeof audio !== 'undefined' && audio) {
         }
     });
 }
+
 
 
 
